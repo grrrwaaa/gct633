619ebd9b655be00e85ac58953b58bc370fcc39ba
grrrwaaa <grrrwaaa@gmail.com>
2013-09-12 14:10:05 +0900

    latest

--- test.lua
+++ test.lua
@@ -1,12 +1,10 @@
-local ffi = require "ffi"
-local len = 44100 * 3
-local buf = ffi.new("float[?]", len)
-
-for i = 0, len-1 do buf[i] = math.sin(i*0.1) * math.sin(i * 0.0001) end
-
 local sndfile = require "audio.sndfile"
+
 local s = sndfile("tmp.wav", "w")
-s:write(buf, len)
+
+local len = 44100 * 3
+for i = 1, len do 
+	s:write(math.random())
+end
 s:close()
 
-print("done")
