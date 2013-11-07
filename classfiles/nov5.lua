51d4d30bd9b0bdc647f4dea925735955ddc1fac4
grrrwaaa <grrrwaaa@gmail.com>
2013-11-07 15:53:44 +0900

    string song

--- classfiles/nov5.lua
+++ classfiles/nov5.lua
@@ -31,9 +31,12 @@ local mynoise = noiseburst(0.1)
 -- play it
 --audio.play(mynoise)
 
+--[[
+local freq = 2000
+
 local delay = {
 	-- create a 1 second block of sample memory:
-	data = buffer(441),
+	data = buffer(samplerate / freq),
 	-- set the initial position to zero:
 	pos = 0,
 }
@@ -56,11 +59,14 @@ audio.play(function()
 	local s = delay.data.samples[ delay.pos ]
 	
 	-- take the average:
-	s = s + 0.3 * (s0 - s)
+	s = s + 0.1 * (s0 - s)
 	  --s * 0.9 + s0 * 0.1
 	  
 	-- overall decay
-	s = s * 0.99
+	s = s * 0.9999 
+	
+	-- bowing:
+	--s = s + 0.01 * math.random() - 0.5)
 	
 	-- scale down & write back in:
 	delay.data.samples[delay.pos] = s
@@ -72,7 +78,111 @@ audio.play(function()
 	return s
 end)
 
+--]]
 
+function make_string(freq)
+
+	local self = {
+		-- create a 1 second block of sample memory:
+		data = buffer(44100),
+		-- set the initial position to zero:
+		pos = 0,
+		-- set the delay period:
+		period = samplerate / freq,
+		
+		filter = 0.1,
+		
+		decay = 0.999,
+		
+		-- pluck input:
+		pluck = 1,
+	}
+	
+	local s0 = 0
+	audio.play(function()
+
+		-- move write head on:
+		self.pos = self.pos + 1
+		-- wrap at end:
+		if self.pos >= self.data.frames then
+			self.pos = 0
+		end
+	
+		-- get read point:
+		local idx = self.pos - self.period
+		if idx < 0 then
+			idx = idx + self.data.frames
+		end
+	
+		-- linear interpolation:
+	
+		local idx0 = math.floor(idx)
+		-- local idx1 = (idx0 % delay.data.frames) + 1
+		local idx1 = idx0 + 1
+		if idx1 >= self.data.frames then
+			idx1 = 0
+		end
+		local v0 = self.data.samples[idx0]
+		local v1 = self.data.samples[idx1]
+		local w1 = idx - idx0
+	
+		-- local s = v0 + w1*(v1 - v0)
+	
+		local w0 = 1 - w1
+		local s = v0 * w0 + v1 * w1
+	
+		-- the filter section:
+		-- take the average:
+		s = s + self.filter * (s0 - s)
+		  --s * 0.9 + s0 * 0.1
+	  
+		-- overall decay
+		s = s * self.decay
+	
+		-- bowing:
+		--s = s + 0.01 * math.random() - 0.5)
+		
+		-- add noise input:
+		s = s + srandom() * self.pluck * 0.1
+		self.pluck = self.pluck * 0.99
+	
+		-- scale down & write back in:
+		self.data.samples[self.pos] = s
+	
+		-- update s0:
+		s0 = s
+	
+	
+	
+		-- hear it:
+		return s
+	end)
+	
+	return self
+end
+
+
+local harp = {}
+for i = 1, 20 do
+	harp[i] = make_string(100 * (i + srandom() * 0.01))
+end
+
+local count = 0
+audio.play(function()
+	
+	count = count + 1
+	if count > samplerate/4 then
+		count = 0
+		
+		local h = harp[math.random(#harp)]
+		
+		h.pluck = 1
+		h.filter = (1-math.random())*(1-math.random())
+	end
+	
+	
+	return 0
+end)
 
 
 
