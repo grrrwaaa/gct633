465fff5f8c1a8167293a24ffa046668fb6faba62
grrrwaaa <grrrwaaa@gmail.com>
2013-12-17 15:17:05 +0900

    improved input example

---
+++ scope_input.lua
@@ -0,0 +1,14 @@
+local audio = require "audio"
+-- adjust this to set the IO latency (in seconds)
+--audio.latency(0.05)
+
+-- this may cause feedback!!
+function route_input(l, r)
+	return l, r
+end
+
+
+audio.play(route_input)
+
+-- show it:
+audio.scope()
